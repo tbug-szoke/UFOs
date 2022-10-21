1447eb12665a87e3a5e54e4b1b294f059d7e7c4b
Theresa Szoke <theresa.szoke@gmail.com>
2022-10-21 19:23:44 -0400

    updated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -25,3 +25,21 @@ function buildTable(data) {
     });
 
 }
+
+// Create function to filter data
+function handleClick() {
+        // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // check to see if a date was entered and filter the data using that date
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // Rebuild the table using the filtered data
+    buildTable(filteredData);)
+}
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
